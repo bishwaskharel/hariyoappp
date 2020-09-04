1ef39f35077fa76dbce0d3e662fa229881428b31
bishwaskharel <bishukharel98@gmail.com>
2020-09-04 18:07:24 +0545

    log in link problem

--- src/components/Admin/Admin.component.jsx
+++ src/components/Admin/Admin.component.jsx
@@ -1,16 +1,31 @@
 import React from 'react'
 import "./Admin.styles.scss"
+import { useState } from 'react';
 
 function Admin() {
-    return (
+     const[itemValue,setItemValue] = useState("");
+     const[imgValue,setImgValue] = useState("");
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setItemValue("");
+        setImgValue("");
+    }
+
+    const handleChange =(e) => {
+        setItemValue(e.target.itemValue);
+        setImgValue(e.target.imgValue);
+    }
+
+   
+    return (
         <div className="admin_Pannel">
         <div className="admin">
             <h1>Add Items</h1>
-            <form type="summit" className="admin">
-            <input placeholder="Enter Item Name" />
-            <input placeholder="Upload Image" />
-            <button>Upload</button>
+            <form onSubmit={handleSubmit} className="admin">
+            <input onChange={handleChange} value={itemValue} placeholder="Enter Item Name" />
+            <input onChange={handleChange} value={imgValue} placeholder="Upload Image" />
+            <button type="submit">Upload</button>
             </form>
         </div>
         <div className="preview">
--- src/components/SignInForm/SignInForm.component.jsx
+++ src/components/SignInForm/SignInForm.component.jsx
@@ -1,17 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./SignInForm.styles.scss"
 import {Link } from "react-router-dom"
 
-function SignInForm() {
-    return (
+const SignInForm = () => {
+    const [value1,setValue1] = useState('');
+    const [value2,setValue2] = useState('');
+
+
+    const handleChange =(e) => {
+        setValue1(e.target.value1);
+       // console.log(value1);
+        setValue2(e.target.value2);
+    }
+
+    const [uploadInfo, setUploadInfo] = useState('Forget Password?')
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+       // console.log(value1);
+       // console.log(value2);
+       // setUploadInfo("You Logged In successfully")
+       /* if (value1 === "admin" && value2 === "hariyo"){
+            const authLink = "/Admin";
+            return 
+        } 
+            console.log("password Incorrenct");
+            const authLink = "/SignInForm";
+            return */
+
+           // setValue1("");
+           // setValue2("");
+            
+        
+
+    }
+
+    const buttonClicked =(e) => {
+       /// console.log(e.target.value1);
+        //console.log(e.target.value2);
+        setUploadInfo("You Logged In successfully")
+        setValue1("");
+        setValue2("");
+       
+    }
+
+return (
         <div className="formBox">
             <h1>Admin Pannel</h1>
-            <form >
-                <input placeholder="User" type="text"/>
-                <input placeholder="Password" type="password" />
-                <Link to="/Admin"><button>Sumbit</button></Link>
+            <form onSubmit={handleSubmit} >
+                <input  value={value1} onChange={handleChange} placeholder="User Name" type="text"/>
+                <input  value={value2} onChange={handleChange} placeholder="Password" type="password" />
+               <Link to={handleSubmit.authLink}><button onClick={buttonClicked} type="submit">Submit</button></Link>
+<p>{uploadInfo}</p>
+                
             </form>
-            
         </div>
     )
 }
